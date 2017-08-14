64e839eca4517fd1578f11147e1989dcb703b357
hubiao <biao.hu@htd.cn>
2017-08-14 10:00:20 +0800

    内容中心访问关联方相关表的方法需要删除，如果有调用的地方，改成调用基础中心

--- basecenter/basecenter-export/src/main/java/cn/htd/basecenter/service/TransactionRelationService.java
+++ basecenter/basecenter-export/src/main/java/cn/htd/basecenter/service/TransactionRelationService.java
@@ -1,8 +1,13 @@
 package cn.htd.basecenter.service;
 
+import java.util.List;
+
 import cn.htd.basecenter.domain.TransactionRelation;
+import cn.htd.basecenter.dto.ImportResultDTO;
 import cn.htd.basecenter.dto.TransactionRelationDTO;
+import cn.htd.common.DataGrid;
 import cn.htd.common.ExecuteResult;
+import cn.htd.common.Pager;
 
 public interface TransactionRelationService{
 	
@@ -20,7 +25,7 @@ public interface TransactionRelationService{
 	 * @param helpDocCategoryDTO
 	 * @return
 	 */
-	ExecuteResult<TransactionRelation> getSingleTransactionRelationByParams(TransactionRelationDTO transactionRelationDTO);
+	ExecuteResult<TransactionRelationDTO> getSingleTransactionRelationByParams(TransactionRelationDTO transactionRelationDTO);
 	
 	/**
 	 * 查询会员是否关联供应商
@@ -28,5 +33,48 @@ public interface TransactionRelationService{
 	 * @return
 	 */
 	ExecuteResult<Boolean> getTransactionRelationIsRelated(String buyerCode);
+	
+	/**
+	 * 添加关联交易
+	 * @param helpDocCategoryDTO
+	 */
+	ExecuteResult<List<String>> addTransactionRelations(List<TransactionRelationDTO> transactionRelationDTOs);
+	
+	/**
+	 * 删除关联交易
+	 * @param transactionRelationDTO
+	 * @return
+	 */
+	ExecuteResult<?> deleteTransactionRelationByParams(TransactionRelationDTO transactionRelationDTO);
+	
+	/**
+	 * 分页查询关联交易
+	 * @param page
+	 * @param helpDocCategoryDTO
+	 * @return
+	 */
+	ExecuteResult<DataGrid<TransactionRelation>> getTransactionRelationForPage(Pager<TransactionRelationDTO> page,TransactionRelationDTO transactionRelationDTO);
+	
+	/**
+	 * 分页查询关联交易
+	 * @param page
+	 * @param helpDocCategoryDTO
+	 * @return
+	 */
+	ExecuteResult<List<TransactionRelationDTO>> getTransactionRelationByParams(TransactionRelationDTO transactionRelationDTO);
+	
+	/**
+	 * 批量取消关联交易
+	 * @param list
+	 * @return
+	 */
+	ExecuteResult<Integer>  cancleRelations(List<String> list);
+	
+	/**
+	 * 导入存储数据
+	 * @param transactionRelationDTOs
+	 * @return
+	 */
+	ExecuteResult<ImportResultDTO> importTransactioRelation(List<TransactionRelationDTO> transactionRelationDTOs);
 	
 }
--- basecenter/basecenter-service/src/main/java/cn/htd/basecenter/service/impl/TransactionRelationServiceImpl.java
+++ basecenter/basecenter-service/src/main/java/cn/htd/basecenter/service/impl/TransactionRelationServiceImpl.java
@@ -1,16 +1,26 @@
 package cn.htd.basecenter.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.Resource;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import cn.htd.basecenter.common.constant.HelpDocConstants;
 import cn.htd.basecenter.dao.TransactionRelationDAO;
 import cn.htd.basecenter.domain.TransactionRelation;
+import cn.htd.basecenter.dto.ImportResultDTO;
 import cn.htd.basecenter.dto.TransactionRelationDTO;
 import cn.htd.basecenter.service.TransactionRelationService;
+import cn.htd.common.DataGrid;
 import cn.htd.common.ExecuteResult;
+import cn.htd.common.Pager;
+
 import com.alibaba.fastjson.JSONObject;
 
 @Service("transactionRelationService")
@@ -40,24 +50,23 @@ public class TransactionRelationServiceImpl implements TransactionRelationServic
 			result.addErrorMessage(e.toString());
 			result.setResult(null);
 		}
-		
 		return result;
 	}
 
 	
 	@Override
-	public ExecuteResult<TransactionRelation> getSingleTransactionRelationByParams(
+	public ExecuteResult<TransactionRelationDTO> getSingleTransactionRelationByParams(
 			TransactionRelationDTO transactionRelationDTO) {
 		logger.info("TransactionRelationServiceImpl--->getTransactionRelationForPage--->transactionRelationDTO:" + JSONObject.toJSONString(transactionRelationDTO));
 		
-		ExecuteResult<TransactionRelation> result = new ExecuteResult<TransactionRelation>();
+		ExecuteResult<TransactionRelationDTO> result = new ExecuteResult<TransactionRelationDTO>();
 		result.setCode(HelpDocConstants.EXECUTE_RESULT_SUCCESS);
 		result.setResultMessage(HelpDocConstants.EXECUTE_RESULT_SUCCESS_TEXT);
 		
 		try {
-			List<TransactionRelation> transactionRelationList = transactionRelationDAO.getTransactionRelationByParams(transactionRelationDTO);
+			List<TransactionRelationDTO> transactionRelationList = transactionRelationDAO.getTransactionRelationByParams(transactionRelationDTO);
 			if(null != transactionRelationList && transactionRelationList.size() == 1){
-				TransactionRelation transactionRelation = transactionRelationList.get(0);
+				TransactionRelationDTO transactionRelation = transactionRelationList.get(0);
 				result.setResult(transactionRelation);
 			}else{
 				logger.info("TransactionRelationServiceImpl--->getSingleTransactionRelationByParams--->transactionRelationList is more then one!!!  - transactionRelationList size:" + transactionRelationList.size());
@@ -75,33 +84,19 @@ public class TransactionRelationServiceImpl implements TransactionRelationServic
 
 	@Override
 	public ExecuteResult<Boolean> getTransactionRelationIsRelated(String buyerCode) {
-		logger.info("TransactionRelationServiceImpl--->getTransactionRelationIsRelated--->buyerCode:" + buyerCode);
-		
+		logger.info("TransactionRelationServiceImpl--->getTransactionRelationIsRelated--->parmas:" + buyerCode);
 		TransactionRelationDTO transactionRelationDTO = new TransactionRelationDTO();
 		transactionRelationDTO.setBuyerCode(buyerCode);
-		
 		ExecuteResult<Boolean> result = new ExecuteResult<Boolean>();
 		result.setCode(HelpDocConstants.EXECUTE_RESULT_SUCCESS);
 		result.setResultMessage(HelpDocConstants.EXECUTE_RESULT_SUCCESS_TEXT);
+		boolean flag = false;
 		try {	
-			List<TransactionRelation> transactionRelationList  = transactionRelationDAO.getTransactionRelationByParams(transactionRelationDTO);
-			
-			if(null != transactionRelationList && transactionRelationList.size() == 1){
-				TransactionRelation transactionRelation = transactionRelationList.get(0);
-				if(null != transactionRelation){
-					result.setResult(transactionRelation.getIsRelated());
-				}else{
-					logger.info("TransactionRelationServiceImpl--->getSingleTransactionRelationByParams--->transactionRelation is null!!!");
-					result.setResult(Boolean.FALSE);
-				}
-				
-			}else{
-				logger.info("TransactionRelationServiceImpl--->getSingleTransactionRelationByParams--->transactionRelationList is more then one!!!  - transactionRelationList size:" + transactionRelationList.size());
-				result.setCode(HelpDocConstants.EXECUTE_RESULT_FAILED);
-				result.setResultMessage(HelpDocConstants.EXECUTE_RESULT_FAILED_TEXT);
-				result.setResult(Boolean.FALSE);
+			List<TransactionRelationDTO> list  = transactionRelationDAO.getTransactionRelationByParams(transactionRelationDTO);
+			if(CollectionUtils.isNotEmpty(list) && list.get(0) != null && "1".equals(list.get(0).getIsRelated())){
+				flag = true; 
 			}
-			
+			result.setResult(flag);
 		} catch (Exception e) {
 			logger.error("\n 方法[{}]，异常：[{}]", "TransactionRelationServiceImpl-getTransactionRelationIsRelated", e.toString());
 			result.setCode(HelpDocConstants.EXECUTE_RESULT_FAILED);
@@ -113,5 +108,106 @@ public class TransactionRelationServiceImpl implements TransactionRelationServic
 	}
 
 
+	@Transactional
+	@Override
+	public ExecuteResult<List<String>> addTransactionRelations(List<TransactionRelationDTO> transactionRelationDTOs) {
+		logger.info("TransactionRelationServiceImpl--->addTransactionRelation--->parmas:" + JSONObject.toJSONString(transactionRelationDTOs));	
+		// 返回状态 ：0.失败  1.成功
+		ExecuteResult<List<String>> result = new ExecuteResult<List<String>>();
+		result.setCode(HelpDocConstants.EXECUTE_RESULT_SUCCESS);
+		result.setResultMessage(HelpDocConstants.EXECUTE_RESULT_SUCCESS_TEXT);
+		List<String> fails = new ArrayList<String>();
+		try {	
+			for(TransactionRelationDTO dto : transactionRelationDTOs){
+				if(!(buyerIsExistCode(dto , null) || buyerNameIsExist(dto.getBuyerName() , null))){
+					if(transactionRelationDAO.addTransactionRelation(dto) > 0){
+						continue;
+					}
+				}
+				fails.add(dto.getBuyerCode());
+			}
+			result.setResult(fails);
+		} catch (Exception e) {
+			logger.error("\n 方法[{}]，异常：[{}]", "TransactionRelationServiceImpl-addTransactionRelation", e.toString());
+			result.setCode(HelpDocConstants.EXECUTE_RESULT_FAILED);
+			result.setResultMessage(HelpDocConstants.EXECUTE_RESULT_FAILED_TEXT);
+			result.addErrorMessage(e.toString());
+			result.setResult(null);
+		}
+		return result;
+	}
 
+
+	@Override
+	public ExecuteResult<?> deleteTransactionRelationByParams(TransactionRelationDTO transactionRelationDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public ExecuteResult<DataGrid<TransactionRelation>> getTransactionRelationForPage(
+			Pager<TransactionRelationDTO> page, TransactionRelationDTO transactionRelationDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public ExecuteResult<List<TransactionRelationDTO>> getTransactionRelationByParams(
+			TransactionRelationDTO transactionRelationDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public ExecuteResult<Integer> cancleRelations(List<String> list) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public ExecuteResult<ImportResultDTO> importTransactioRelation(
+			List<TransactionRelationDTO> transactionRelationDTOs) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private boolean buyerIsExistCode(TransactionRelationDTO dto , String id){
+		boolean flag = false;
+		TransactionRelationDTO params = new TransactionRelationDTO();
+		List<TransactionRelationDTO> list = null;
+		if(StringUtils.isEmpty(dto.getBuyerCode())){
+			return flag;
+		}
+		params.setBuyerCode(dto.getBuyerCode());
+		list = transactionRelationDAO.getTransactionRelationByParams(params);
+		if(!CollectionUtils.isEmpty(list)){
+			flag = true; 
+			logger.info("buyerIsExist params : {buyerCode : " + params.getBuyerCode() + ", id : "+ id + 
+					",list : " + list.get(0) + "}");
+			if(StringUtils.isNotEmpty(id) && list.get(0) != null && id.equals(list.get(0).getId())){
+				flag = false;
+			}
+		}
+		return flag;
+	}
+
+	private boolean buyerNameIsExist(String buyerName , String id){
+		if(StringUtils.isEmpty(buyerName)){
+			return true;
+		}
+		TransactionRelationDTO params = new TransactionRelationDTO();
+		params.setBuyerName(buyerName);
+		List<TransactionRelationDTO> list = transactionRelationDAO.getTransactionRelationByParams(params);
+		if(!CollectionUtils.isEmpty(list)){
+			if(StringUtils.isNotEmpty(id) && list.get(0) != null && id.equals(list.get(0).getId())){
+				return false; 
+			}
+			return true;
+		}
+		return false;
+	}
 }

643d23e7bc3ae3a4fb4b1cab2012bbad28acb93a
xuxuejiao <1164269669@qq.com>
2017-08-15 19:32:22 +0800

    提交

---
+++ promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/dao/PromotionSampleDAO.java
@@ -0,0 +1,8 @@
+package cn.htd.promotion.cpc.biz.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository("cn.htd.promotion.cpc.biz.dao.promotionSampleDAO")
+public interface PromotionSampleDAO {
+
+}

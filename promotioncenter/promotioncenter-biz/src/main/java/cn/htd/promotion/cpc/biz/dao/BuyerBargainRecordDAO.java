b3056a89702c7c3f33be5b14a13194460ba11329
xuxuejiao <1164269669@qq.com>
2017-08-16 20:47:51 +0800

    提交获取砍价记录

--- promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/dao/BuyerBargainRecordDAO.java
+++ promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/dao/BuyerBargainRecordDAO.java
@@ -1,8 +1,13 @@
 package cn.htd.promotion.cpc.biz.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
+
+import cn.htd.promotion.cpc.biz.dmo.BuyerBargainRecordDMO;
 
 @Repository("cn.htd.promotion.cpc.biz.dao.buyerBargainRecordDAO")
 public interface BuyerBargainRecordDAO {
 
+	public List<BuyerBargainRecordDMO> getBuyerBargainRecordByBargainCode(String bargainCode);
 }

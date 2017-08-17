79e191a2fb27f98b8224348f45ef556e2f15301a
li.jun <li.jun@htd.cn>
2017-08-17 08:30:37 +0800

    增加代码备注，方便别人理解

--- membercenter/membercenter-service/src/main/java/cn/htd/membercenter/service/impl/MemberSuperBossServiceImpl.java
+++ membercenter/membercenter-service/src/main/java/cn/htd/membercenter/service/impl/MemberSuperBossServiceImpl.java
@@ -26,6 +26,11 @@ public class MemberSuperBossServiceImpl implements MemberSuperBossService {
 	@Resource
 	private MemberSuperBossDao memberSuperBossDao;
 
+	/**
+	 * @desc 
+	 * cooperateVerifyStatus:供应商审核状态 1：待审核，2：已通过，3：被驳回
+	 * verifyStatus：运营审核状态 1：待审核，2：已通过，3：被驳回
+	 */
 	@Override
 	public ExecuteResult<DataGrid<MemberBaseInfoDTO>> selectMemberByCustmanagerCode(String managerCode, Pager pager) {
 		logger.info("MemberSuperBossServiceImpl===selectMemberByCustmanagerCode服务执行开始，参数：managerCode:" + managerCode
@@ -37,22 +42,22 @@ public class MemberSuperBossServiceImpl implements MemberSuperBossService {
 			if (count > 0) {
 				List<MemberBaseInfoDTO> resList = memberSuperBossDao.selectMemberByCustmanagerCode(managerCode, pager);
 				if( null !=resList && resList.size()>0){
-					for(MemberBaseInfoDTO memberbase:resList ){ //managerstatus 1.运营待审核 2.运营审核不通过 3.运营审核驳回 4.供应商待审核 5.供应商审核通过
-						if(StringUtils.isNotBlank(memberbase.getCooperateVerifyStatus())){
-							if(memberbase.getCooperateVerifyStatus().equals("1")){
-								memberbase.setManagerStatus("4");
+					for(MemberBaseInfoDTO memberbase:resList ){ //managerstatus 1.运营待审核 2.运营审核通过 3.运营审核不通过/驳回 4.供应商待审核 5.供应商审核通过
+						if(StringUtils.isNotBlank(memberbase.getCooperateVerifyStatus())){ //如果供应商审核状态不为空，则说明运营已经审核通过
+							if(memberbase.getCooperateVerifyStatus().equals("1")){//如果cooperateVerifyStatus为1，即供应商待审核状态
+								memberbase.setManagerStatus("4");//设置超级经理人展示会员注册状态，4，表示供应商待审核
 							}else{
-								memberbase.setManagerStatus("5");
+								memberbase.setManagerStatus("5");//cooperateVerifyStatus为其他状态，说明供应商审核通过，供应商审核驳回，业务要求默认审核通过
 							}
 						}else{
-							if(StringUtils.isNotBlank(memberbase.getVerifyStatus())){
-								if(memberbase.getVerifyStatus().equals("1")){
+							if(StringUtils.isNotBlank(memberbase.getVerifyStatus())){ //如果是运行审核，则判断超级经理级展示状态
+								if(memberbase.getVerifyStatus().equals("1")){//verifyStatus为1.说明运营待审核
 									memberbase.setManagerStatus("1");
 								}
-								if(memberbase.getVerifyStatus().equals("2")){
+								if(memberbase.getVerifyStatus().equals("2")){//2.说明运营审核通过
 									memberbase.setManagerStatus("2");
 								}
-								if(memberbase.getVerifyStatus().equals("3")){
+								if(memberbase.getVerifyStatus().equals("3")){//3.运行审核不通过
 									memberbase.setManagerStatus("3");
 								}
 							}

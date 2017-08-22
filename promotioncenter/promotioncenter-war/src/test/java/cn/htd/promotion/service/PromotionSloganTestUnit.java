657f5c994994b32f41dfe80ce45660278b84e966
mac <mz.xu@htd.cn>
2017-08-22 16:36:17 +0800

    no message

--- promotioncenter/promotioncenter-war/src/test/java/cn/htd/promotion/service/PromotionSloganTestUnit.java
+++ promotioncenter/promotioncenter-war/src/test/java/cn/htd/promotion/service/PromotionSloganTestUnit.java
@@ -1,6 +1,11 @@
 package cn.htd.promotion.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Resource;
+
+import net.sf.json.JSONObject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,8 +13,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import cn.htd.promotion.cpc.biz.dao.PromotionSloganDAO;
 import cn.htd.promotion.cpc.biz.service.PromotionSloganService;
+
+import com.alibaba.fastjson.JSON;
 
 @Transactional  
 @RunWith(SpringJUnit4ClassRunner.class)  
@@ -18,8 +24,9 @@ public class PromotionSloganTestUnit {
 
     @Resource
     private PromotionSloganService promotionSloganService;
+    
 	@Test
 	public void savePromotionInfo() {
-		
+		 
 	}
 }

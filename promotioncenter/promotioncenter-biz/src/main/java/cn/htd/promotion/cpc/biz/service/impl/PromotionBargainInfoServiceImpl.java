c0922a0075124041ae7acba3add1f5d64d9cc572
mac <mz.xu@htd.cn>
2017-08-24 21:58:42 +0800

    no message

--- promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/impl/PromotionBargainInfoServiceImpl.java
+++ promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/impl/PromotionBargainInfoServiceImpl.java
@@ -48,12 +48,6 @@ import cn.htd.promotion.cpc.dto.response.PromotionValidDTO;
 import cn.htd.promotion.cpc.dto.response.PromotonInfoResDTO;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 
 @Service("promotionBargainInfoService")
 public class PromotionBargainInfoServiceImpl implements
--- promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/impl/PromotionInfoServiceImpl.java
+++ promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/impl/PromotionInfoServiceImpl.java
@@ -1,14 +1,10 @@
 package cn.htd.promotion.cpc.biz.service.impl;
 
 import java.util.List;
-
-
 import javax.annotation.Resource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
 import cn.htd.common.DataGrid;
 import cn.htd.common.Pager;
 import cn.htd.promotion.cpc.biz.dao.PromotionInfoDAO;

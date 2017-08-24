15cd5aa44b1dac8f292cbff7a6ca61c4388c781c
jy.tang <jy.tang@htd.cn>
2017-08-24 14:56:06 +0800

    枚举类型

--- promotioncenter/promotioncenter-api/src/main/java/cn/htd/promotion/api/impl/AwardRecordAPIImpl.java
+++ promotioncenter/promotioncenter-api/src/main/java/cn/htd/promotion/api/impl/AwardRecordAPIImpl.java
@@ -45,8 +45,8 @@ public class AwardRecordAPIImpl implements AwardRecordAPI {
                     result.setResultMessage(ResultCodeEnum.SUCCESS.getMsg());
                 }
             }else{
-                result.setCode(ResultCodeEnum.PROMOTION_PARAM_IS_NULL.getCode());
-                result.setErrorMessage(ResultCodeEnum.PROMOTION_PARAM_IS_NULL.getMsg());
+                result.setCode(ResultCodeEnum.PROMOTION_AWARD_IS_NULL.getCode());
+                result.setErrorMessage(ResultCodeEnum.PROMOTION_AWARD_IS_NULL.getMsg());
             }
 
             logger.info("promotionId{}:AwardRecordAPIImpl.getAwardRecordByPromotionId（）方法,出参{}",messageId , dto.getPromotionId(), JSON.toJSONString(result));

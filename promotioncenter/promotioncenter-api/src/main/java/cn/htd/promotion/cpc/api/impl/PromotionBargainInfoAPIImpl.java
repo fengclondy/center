185b88b5bf390f5c459d2ee73a71ca702fac466d
xuxuejiao <1164269669@qq.com>
2017-08-25 09:56:55 +0800

    修改删除的代码

--- promotioncenter/promotioncenter-api/src/main/java/cn/htd/promotion/cpc/api/impl/PromotionBargainInfoAPIImpl.java
+++ promotioncenter/promotioncenter-api/src/main/java/cn/htd/promotion/cpc/api/impl/PromotionBargainInfoAPIImpl.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import cn.htd.common.DataGrid;
+import cn.htd.common.Pager;
 import cn.htd.promotion.cpc.api.PromotionBargainInfoAPI;
 import cn.htd.promotion.cpc.biz.service.PromotionBargainInfoService;
 import cn.htd.promotion.cpc.common.emums.ResultCodeEnum;
@@ -15,6 +17,9 @@ import cn.htd.promotion.cpc.common.util.ValidateResult;
 import cn.htd.promotion.cpc.common.util.ValidationUtils;
 import cn.htd.promotion.cpc.dto.request.BuyerBargainLaunchReqDTO;
 import cn.htd.promotion.cpc.dto.response.PromotionBargainInfoResDTO;
+import cn.htd.promotion.cpc.dto.response.PromotionBargainOverviewResDTO;
+import cn.htd.promotion.cpc.dto.response.PromotonInfoResDTO;
+
 import com.alibaba.fastjson.JSON;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
@@ -63,6 +68,7 @@ public class PromotionBargainInfoAPIImpl implements PromotionBargainInfoAPI {
         return result;
     }
 
+    @Override
     public ExecuteResult<List<PromotionBargainInfoResDTO>> getPromotionBargainInfoList(String messageId,
             String promotionId) {
         ExecuteResult<List<PromotionBargainInfoResDTO>> result = new ExecuteResult<List<PromotionBargainInfoResDTO>>();
@@ -74,6 +80,32 @@ public class PromotionBargainInfoAPIImpl implements PromotionBargainInfoAPI {
         }
         return result;
     }
+    
+    @Override
+    public ExecuteResult<DataGrid<PromotonInfoResDTO>> queryPromotionInfoListBySellerCode(
+			String sellerCode, Pager<String> page) {
+		ExecuteResult<DataGrid<PromotonInfoResDTO>> result = new ExecuteResult<DataGrid<PromotonInfoResDTO>>();
+		if(!StringUtils.isEmpty(sellerCode)){
+			return promotionBargainInfoService.queryPromotionInfoListBySellerCode(sellerCode, page);
+		}else{
+			result.setCode(ResultCodeEnum.PROMOTION_PARAM_IS_NULL.getCode());
+			result.setErrorMessage(ResultCodeEnum.PROMOTION_PARAM_IS_NULL.getMsg());
+		}
+		return result;
+	}
+    
+    @Override
+    public ExecuteResult<DataGrid<PromotionBargainOverviewResDTO>> queryPromotionBargainOverview(
+			String sellerCode,  Pager<String> page) {
+		ExecuteResult<DataGrid<PromotionBargainOverviewResDTO>> result = new ExecuteResult<DataGrid<PromotionBargainOverviewResDTO>>();
+		if(!StringUtils.isEmpty(sellerCode)){
+			return promotionBargainInfoService.queryPromotionBargainOverview(sellerCode, page);
+		}else{
+			result.setCode(ResultCodeEnum.PROMOTION_PARAM_IS_NULL.getCode());
+			result.setErrorMessage(ResultCodeEnum.PROMOTION_PARAM_IS_NULL.getMsg());
+		}
+		return result;
+	}
 
 
 }

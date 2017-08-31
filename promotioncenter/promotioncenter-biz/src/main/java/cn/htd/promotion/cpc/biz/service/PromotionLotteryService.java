d16a820fc4c787ef2b37b9082349aeb7f56c1ac5
jiangkun <jiaboy20@sina.com>
2017-08-31 15:12:08 +0800

    扭蛋修改代码

--- promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/PromotionLotteryService.java
+++ promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/PromotionLotteryService.java
@@ -1,6 +1,21 @@
 package cn.htd.promotion.cpc.biz.service;
 
+import cn.htd.promotion.cpc.dto.request.DrawLotteryReqDTO;
+import cn.htd.promotion.cpc.dto.response.DrawLotteryResDTO;
+
 public interface PromotionLotteryService {
 
+    /**
+     * 开始抽奖处理
+     * @param requestDTO
+     * @return
+     */
+    public DrawLotteryResDTO beginDrawLottery(DrawLotteryReqDTO requestDTO);
 
+    /**
+     * 查询抽奖结果处理
+     * @param requestDTO
+     * @return
+     */
+    public DrawLotteryResDTO getDrawLotteryResult(DrawLotteryReqDTO requestDTO);
 }
--- promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/impl/PromotionLotteryServiceImpl.java
+++ promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/impl/PromotionLotteryServiceImpl.java
@@ -1,6 +1,8 @@
 package cn.htd.promotion.cpc.biz.service.impl;
 
 import cn.htd.promotion.cpc.biz.service.PromotionLotteryService;
+import cn.htd.promotion.cpc.dto.request.DrawLotteryReqDTO;
+import cn.htd.promotion.cpc.dto.response.DrawLotteryResDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -10,4 +12,25 @@ public class PromotionLotteryServiceImpl implements PromotionLotteryService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PromotionLotteryServiceImpl.class);
 
+    /**
+     * 开始抽奖处理
+     *
+     * @param requestDTO
+     * @return
+     */
+    @Override
+    public DrawLotteryResDTO beginDrawLottery(DrawLotteryReqDTO requestDTO) {
+        return null;
+    }
+
+    /**
+     * 查询抽奖结果处理
+     *
+     * @param requestDTO
+     * @return
+     */
+    @Override
+    public DrawLotteryResDTO getDrawLotteryResult(DrawLotteryReqDTO requestDTO) {
+        return null;
+    }
 }

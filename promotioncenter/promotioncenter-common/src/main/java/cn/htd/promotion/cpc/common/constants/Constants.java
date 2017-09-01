92bbb5256a69ed9fe954715818f25e41e67b9504
haozhiyuan <zy.hao@htd.cn>
2017-09-01 18:00:59 +0800

    汇掌柜秒杀代码提交

--- promotioncenter/promotioncenter-common/src/main/java/cn/htd/promotion/cpc/common/constants/Constants.java
+++ promotioncenter/promotioncenter-common/src/main/java/cn/htd/promotion/cpc/common/constants/Constants.java
@@ -8,6 +8,18 @@ package cn.htd.promotion.cpc.common.constants;
  *
  */
 public class Constants {
-	
+
 	public final static String ORDER_QUERY = "OrderQuery";
+
+	// 秒杀库存锁定操作
+	public final static String SECKILL_RESERVE = "1";
+
+	// 秒杀库存释放操作
+	public final static String SECKILL_RELEASE = "2";
+
+	// 秒杀库存扣减操作
+	public final static String SECKILL_REDUCE = "3";
+
+	// 秒杀库存回滚操作
+	public final static String SECKILL_ROLLBACK = "4";
 }
--- promotioncenter/promotioncenter-common/src/main/java/cn/htd/promotion/cpc/common/constants/RedisConst.java
+++ promotioncenter/promotioncenter-common/src/main/java/cn/htd/promotion/cpc/common/constants/RedisConst.java
@@ -19,25 +19,25 @@ public class RedisConst {
 	public static final String REDIS_BARGAIN_REAL_REMAIN_COUNT = "REAL_REMAIN_COUNT";
 	// Redis真实参与砍价人数
 	public static final String REDIS_BARGAIN_REAL_ACTOR_COUNT = "REAL_ACTOR_COUNT";
-	//Redis砍价活动开始时间
+	// Redis砍价活动开始时间
 	public static final String REDIS_BARGAIN_START_TIME = "BARGAIN_START_TIME";
-	//Redis砍价活动结束时间 
+	// Redis砍价活动结束时间
 	public static final String REDIS_BARGAIN_END_TIME = "BARGAIN_END_TIME";
-	//Redis用户参加砍价活动信息
-	public static final String REDIS_BUYER_BARGAIN_USELOG =  "BUYER_BARGAIN_USELOG";
-	//Redis砍价活动金额拆分
+	// Redis用户参加砍价活动信息
+	public static final String REDIS_BUYER_BARGAIN_USELOG = "BUYER_BARGAIN_USELOG";
+	// Redis砍价活动金额拆分
 	public static final String REDIS_BARGAIN_PRICE_SPLIT = "BARGAIN_PRICE_SPLIT";
-	
+
 	public static final String BUYER_BARGAIN_RECORD = "BUYER_BARGAIN_RECORD";
 
 	// Redis抽奖活动有效信息
 	public static final String REDIS_LOTTERY_VALID = "B2B_MIDDLE_LOTTERY_VALID";
 	// Redis抽奖活动信息
-	public static final String REDIS_LOTTERY_INFO= "B2B_MIDDLE_LOTTERY_INFO";
+	public static final String REDIS_LOTTERY_INFO = "B2B_MIDDLE_LOTTERY_INFO";
 	// Redis抽奖次数信息
-	public static final String REDIS_LOTTERY_TIMES_INFO="B2B_MIDDLE_LOTTERY_TIMES_INFO";
+	public static final String REDIS_LOTTERY_TIMES_INFO = "B2B_MIDDLE_LOTTERY_TIMES_INFO";
 	// Redis抽奖活动奖品中奖概率
-	public static final String REDIS_LOTTERY_AWARD_WINNING_PERCENTAGE= "AWARD_WINNING_PERCENTAGE";
+	public static final String REDIS_LOTTERY_AWARD_WINNING_PERCENTAGE = "AWARD_WINNING_PERCENTAGE";
 	// Redis抽奖活动每日抽奖次数
 	public static final String REDIS_LOTTERY_BUYER_DAILY_DRAW_TIMES = "BUYER_DAILY_DRAW_TIMES";
 	// Redis抽奖活动每日中奖次数
@@ -63,31 +63,32 @@ public class RedisConst {
 	// Redis抽奖活动卖家每日中奖次数
 	public static final String REDIS_LOTTERY_SELLER_WINED_TIMES = "B2B_MIDDLE_LOTTERY_SELLER_WINED_TIMES";
 
-
-    // Redis会员促销活动有效信息
-    public static final String PROMOTION_REDIS_TIMELIMITED_VALID = "PROMOTION_B2C_MIDDLE_TIMELIMITED_VALID";
-    // Redis秒杀活动数据
-    public static final String PROMOTION_REDIS_TIMELIMITED = "PROMOTION_B2C_MIDDLE_TIMELIMITED";
-    // Redis秒杀活动结果数据
-    public static final String PROMOTION_REDIS_TIMELIMITED_RESULT = "PROMOTION_B2C_MIDDLE_TIMELIMITED_RESULT";
-    // Redis秒杀活动数据
-    public static final String PROMOTION_REDIS_TIMELIMITED_INDEX = "PROMOTION_B2C_MIDDLE_TIMELIMITED_INDEX";
-    // Redis秒杀商品总数量
-    public static final String PROMOTION_REDIS_TIMELIMITED_TOTAL_COUNT = "PROMOTION_B2C_TOTAL_COUNT";
-    // Redis展示已秒杀商品数量
-    public static final String PROMOTION_REDIS_TIMELIMITED_SHOW_REMAIN_COUNT = "PROMOTION_B2C_SHOW_REMAIN_COUNT";
-    // Redis展示参与秒杀人数
-    public static final String PROMOTION_REDIS_TIMELIMITED_SHOW_ACTOR_COUNT = "PROMOTION_B2C_SHOW_ACTOR_COUNT";
-    // Redis真实秒杀商品数量
-    public static final String PROMOTION_REDIS_TIMELIMITED_REAL_REMAIN_COUNT = "PROMOTION_B2C_REAL_REMAIN_COUNT";
-    // Redis真实参与秒杀人数
-    public static final String PROMOTION_REDIS_TIMELIMITED_REAL_ACTOR_COUNT = "PROMOTION_B2C_REAL_ACTOR_COUNT";
-    // Redis会员秒杀活动参加信息
-    public static final String PROMOTION_REDIS_BUYER_TIMELIMITED_COUNT = "PROMOTION_B2C_MIDDLE_BUYER_TIMELIMITED_COUNT";
-    // Redis会员秒杀log信息
-    public static final String PROMOTION_REDIS_BUYER_TIMELIMITED_USELOG = "PROMOTION_B2B_MIDDLE_BUYER_TIMELIMITED_USELOG";
-    // Redis会员秒杀的使用log信息
-    public static final String PROMOTION_REDIS_BUYER_TIMELIMITED_NEED_SAVE_USELOG = "PROMOTION_B2B_MIDDLE_BUYER_TIMELIMITED_NEED_SAVE_USELOG";
-    //扭蛋活动前缀
-    public static final String REDIS_GASHAPON_PREFIX = "GASHAPON_";
+	// Redis会员促销活动有效信息
+	public static final String PROMOTION_REDIS_TIMELIMITED_VALID = "PROMOTION_B2C_MIDDLE_TIMELIMITED_VALID";
+	// Redis秒杀活动数据
+	public static final String PROMOTION_REDIS_TIMELIMITED = "PROMOTION_B2C_MIDDLE_TIMELIMITED";
+	// Redis秒杀活动结果数据
+	public static final String PROMOTION_REDIS_TIMELIMITED_RESULT = "PROMOTION_B2C_MIDDLE_TIMELIMITED_RESULT";
+	// Redis秒杀活动数据
+	public static final String PROMOTION_REDIS_TIMELIMITED_INDEX = "PROMOTION_B2C_MIDDLE_TIMELIMITED_INDEX";
+	// Redis秒杀商品总数量
+	public static final String PROMOTION_REDIS_TIMELIMITED_TOTAL_COUNT = "PROMOTION_B2C_TOTAL_COUNT";
+	// Redis展示已秒杀商品数量
+	public static final String PROMOTION_REDIS_TIMELIMITED_SHOW_REMAIN_COUNT = "PROMOTION_B2C_SHOW_REMAIN_COUNT";
+	// Redis展示参与秒杀人数
+	public static final String PROMOTION_REDIS_TIMELIMITED_SHOW_ACTOR_COUNT = "PROMOTION_B2C_SHOW_ACTOR_COUNT";
+	// Redis真实秒杀商品数量
+	public static final String PROMOTION_REDIS_TIMELIMITED_REAL_REMAIN_COUNT = "PROMOTION_B2C_REAL_REMAIN_COUNT";
+	// Redis真实参与秒杀人数
+	public static final String PROMOTION_REDIS_TIMELIMITED_REAL_ACTOR_COUNT = "PROMOTION_B2C_REAL_ACTOR_COUNT";
+	// Redis会员秒杀活动参加信息
+	public static final String PROMOTION_REDIS_BUYER_TIMELIMITED_COUNT = "PROMOTION_B2C_MIDDLE_BUYER_TIMELIMITED_COUNT";
+	// Redis会员秒杀log信息
+	public static final String PROMOTION_REDIS_BUYER_TIMELIMITED_USELOG = "PROMOTION_B2B_MIDDLE_BUYER_TIMELIMITED_USELOG";
+	// Redis会员秒杀的使用log信息
+	public static final String PROMOTION_REDIS_BUYER_TIMELIMITED_NEED_SAVE_USELOG = "PROMOTION_B2B_MIDDLE_BUYER_TIMELIMITED_NEED_SAVE_USELOG";
+	// Redis秒杀订单信息
+	public static final String PROMOTION_REDIS_BUYER_TIMELIMITED_ORDER_INFO = "PROMOTION_B2B_MIDDLE_BUYER_TIMELIMITED_ORDER_INFO";
+	// 扭蛋活动前缀
+	public static final String REDIS_GASHAPON_PREFIX = "GASHAPON_";
 }

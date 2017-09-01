77f7435da6c2ace5e9b7551cd4032daec734ce33
zhangding <ding.zhang@htd.cn>
2017-09-01 09:59:33 +0800

    修改枚举

--- promotioncenter/promotioncenter-common/src/main/java/cn/htd/promotion/cpc/common/emums/ResultCodeEnum.java
+++ promotioncenter/promotioncenter-common/src/main/java/cn/htd/promotion/cpc/common/emums/ResultCodeEnum.java
@@ -29,15 +29,15 @@ public enum ResultCodeEnum {
     PROMOTION_NO_STOCK("26009", "促销活动商品库存不足"),
     POMOTION_SPLIT_PRICE_ERROR("26010", "促销活动拆分金额失败"),
     PROMOTION_BARGAIN_JOIN_QTY("26011", "促销活动参与次数已上限"),
-
+    BARGAIN_NOT_UNAVAILABLE("26012","砍价活动不可用"),
 
     LOTTERY_NO_RESULT("26200", "抽奖进行中请继续等待"),
     LUCK_DRAW_PARAM_IS_NULL("26201", "调用扭蛋抽奖接口入参不能为空！"),
     LUCK_DRAW_NOT_HAVE_DRAW_PERMISSION("26202", "该会员店没有抽奖权限"),
     LOTTERY_NOT_IN_TIME_INTERVAL("26203", "抽奖活动不在当前时间段内"),
-    LOTTERY_BUYER_NO_AUTHIORITY("25204", "粉丝没有抽奖权限"),
-    LOTTERY_SELLER_NO_AUTHIORITY("25205", "会员店没有参加本次抽奖活动"),
-    PROMOTION_LOTTERY_NO_DESCRIBE_CONTENT("25206","没有查询到活动规则");
+    LOTTERY_BUYER_NO_AUTHIORITY("26204", "粉丝没有抽奖权限"),
+    LOTTERY_SELLER_NO_AUTHIORITY("26205", "会员店没有参加本次抽奖活动"),
+    PROMOTION_LOTTERY_NO_DESCRIBE_CONTENT("26206","没有查询到活动规则");
 
     private ResultCodeEnum(String code, String msg) {
         this.code = code;

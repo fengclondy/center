b6d48dbecf7330e0afeceb703e8f1aac17c55e5b
Shi Hongbo <hb.shi@htd.cn>
2017-09-02 20:30:12 +0800

    启动异常修复（暂时注掉秒杀相关接口）

--- promotioncenter/promotioncenter-api/src/main/java/cn/htd/promotion/cpc/api/impl/PromotionTimelimitedInfoAPIImpl.java
+++ promotioncenter/promotioncenter-api/src/main/java/cn/htd/promotion/cpc/api/impl/PromotionTimelimitedInfoAPIImpl.java
@@ -25,7 +25,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-@Service("promotionTimelimitedInfoAPI")
+//@Service("promotionTimelimitedInfoAPI")
 public class PromotionTimelimitedInfoAPIImpl {
 //implements PromotionTimelimitedInfoAPI {
 //	

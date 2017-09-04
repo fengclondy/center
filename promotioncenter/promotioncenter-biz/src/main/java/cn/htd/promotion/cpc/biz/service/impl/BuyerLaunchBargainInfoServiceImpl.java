724cbc8052cb0088043190016439821f64dbd6aa
xuxuejiao <1164269669@qq.com>
2017-09-04 21:18:07 +0800

    提交修改当前价

--- promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/impl/BuyerLaunchBargainInfoServiceImpl.java
+++ promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/impl/BuyerLaunchBargainInfoServiceImpl.java
@@ -447,6 +447,7 @@ public class BuyerLaunchBargainInfoServiceImpl implements BuyerLaunchBargainInfo
 							  //更新发起砍价表
 							  buyerBargainLaunchReqDTO.setBargainOverTime(new Date());
 							  buyerBargainLaunchReqDTO.setIsBargainOver(1);
+							  buyerLaunchBargainInfoDAO.updateBuyerLaunchBargainInfo(buyerBargainLaunchReqDTO);
 							    BuyerLaunchBargainInfoResDTO buyerLaunchBargainInfo11 = new BuyerLaunchBargainInfoResDTO();
 							    buyerLaunchBargainInfo11.setPromotionId(promotionId);
 							    buyerLaunchBargainInfo11.setLevelCode(levelCode);
@@ -464,10 +465,11 @@ public class BuyerLaunchBargainInfoServiceImpl implements BuyerLaunchBargainInfo
 								    		}
 							    		}
 							    	}
+							    	System.out.println(JSON.toJSONString(listPromotions));
 							    	promotionBargainRedisHandle.addBargainInfo2Redis(listPromotions);
 							    }
 						  }
-						  buyerLaunchBargainInfoDAO.updateBuyerLaunchBargainInfo(buyerBargainLaunchReqDTO);
+						  
 //						  String buyerBargainLaunchJson = JSON.toJSONString(buyerBargainLaunchReqDTO);
 //						  promotionRedisDB.tailPush(Constants.BUYER_LAUNCH_BARGAIN_INFO, buyerBargainLaunchJson);//从右边插入队列
 						  //插入砍价记录

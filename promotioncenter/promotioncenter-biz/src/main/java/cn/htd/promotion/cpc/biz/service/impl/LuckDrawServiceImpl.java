e557d70b73c5ddc55e0d4dc7f7ad810be83a8141
ting.jiang <ting.jiang@htd.cn>
2017-09-11 15:38:00 +0800

    bug

--- promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/impl/LuckDrawServiceImpl.java
+++ promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/impl/LuckDrawServiceImpl.java
@@ -11,6 +11,15 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
 import cn.htd.common.constant.DictionaryConst;
 import cn.htd.common.util.DictionaryUtils;
 import cn.htd.common.util.SysProperties;
@@ -47,13 +56,6 @@ import cn.htd.promotion.cpc.dto.response.PromotionSellerRuleDTO;
 import cn.htd.promotion.cpc.dto.response.PromotionStatusHistoryDTO;
 import cn.htd.promotion.cpc.dto.response.ShareLinkHandleResDTO;
 import cn.htd.promotion.cpc.dto.response.ValidateLuckDrawResDTO;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 
 @Service("luckDrawService")
 public class LuckDrawServiceImpl implements LuckDrawService {
@@ -455,7 +457,7 @@ public class LuckDrawServiceImpl implements LuckDrawService {
                     promotionAwardInfoDAO.update(padDTO);
                 }
             }
-            promotionLotteryCommonService.initPromotionLotteryRedisInfoWithThread(result);
+            promotionLotteryCommonService.initPromotionLotteryRedisInfo(result);
             result = viewDrawLotteryInfo(promotionInfoEditReqDTO.getPromotionId());
             result.setResponseCode(ResultCodeEnum.SUCCESS.getCode());
             result.setResponseMsg(ResultCodeEnum.SUCCESS.getMsg());

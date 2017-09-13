90593116c3a61fbeea9c9baf4ffc777f6748c5e8
li.jun <li.jun@htd.cn>
2017-09-13 10:58:11 +0800

    job 代码优化

--- promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/dao/PromotionTimelimitedInfoDAO.java
+++ promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/dao/PromotionTimelimitedInfoDAO.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
+import cn.htd.common.Pager;
 import cn.htd.promotion.cpc.biz.dmo.TimelimitedInfoDMO;
 import cn.htd.promotion.cpc.dto.request.TimelimitedInfoReqDTO;
+import cn.htd.promotion.cpc.dto.response.PromotionInfoDTO;
 import cn.htd.promotion.cpc.dto.response.PromotionSellerDetailDTO;
 import cn.htd.promotion.cpc.dto.response.TimelimitedInfoResDTO;
 
@@ -15,5 +17,9 @@ public interface PromotionTimelimitedInfoDAO {
     List<TimelimitedInfoResDTO> selectTimelimitedInfo(@Param("buyerCode") String buyerCode);
     
     List<PromotionSellerDetailDTO> selectPromotionSellerDetailInfo(@Param("promotionId") String promotionId,@Param("buyerCode") String buyerCode);
+
+    public List<PromotionInfoDTO> queryPromotionList(@Param("entity") PromotionInfoDTO condition, @Param("page") Pager<PromotionInfoDTO> page);
+    
+    public void updatePromotionShowStatusById(PromotionInfoDTO promotionInfo);
 
 }

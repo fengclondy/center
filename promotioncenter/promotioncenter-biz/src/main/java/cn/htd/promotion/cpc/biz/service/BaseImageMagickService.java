d1a7a0dca38245a97ebc58875087bbe7a344a04a
ting.jiang <ting.jiang@htd.cn>
2017-09-22 14:59:20 +0800

    图片处理

---
+++ promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/BaseImageMagickService.java
@@ -0,0 +1,9 @@
+package cn.htd.promotion.cpc.biz.service;
+
+import cn.htd.promotion.cpc.dto.request.BaseImageDTO;
+
+public interface BaseImageMagickService {
+
+	public String margeImage(BaseImageDTO images);
+
+}

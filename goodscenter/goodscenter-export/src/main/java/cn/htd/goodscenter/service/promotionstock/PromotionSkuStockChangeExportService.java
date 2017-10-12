1342eed775e8fad52d3dee19db27166a1bc6eb3c
mac <mz.xu@htd.cn>
2017-10-12 20:40:42 +0800

    no message

--- goodscenter/goodscenter-export/src/main/java/cn/htd/goodscenter/service/promotionstock/PromotionSkuStockChangeExportService.java
+++ goodscenter/goodscenter-export/src/main/java/cn/htd/goodscenter/service/promotionstock/PromotionSkuStockChangeExportService.java
@@ -8,14 +8,14 @@ public interface PromotionSkuStockChangeExportService {
 
     /**
      * 批量扣减库存 （活动中心，扣减库存）
-     * 注：只针对内部供应商和外部供应商商品
+     * 注：只针对外部供应商商品
      * @return
      */
     ExecuteResult<String> batchReduceStock(List<PromotionStockChangeDTO> promotionStockChangeDTOs);
     
     /**
      * 释放库存 （活动中心，取消订单）
-     * 注：只针对内部供应商和外部供应商商品
+     * 注：只针对外部供应商商品
      * @return
      */
     ExecuteResult<String> batchReleaseStock(List<PromotionStockChangeDTO> promotionStockChangeDTOs);

4e3b7f222c35fecf59026bb1f710821bbdc148e7
zhangding <ding.zhang@htd.cn>
2017-10-16 09:22:06 +0800

    错别字修改

--- promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/impl/PromotionLotteryCommonServiceImpl.java
+++ promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/impl/PromotionLotteryCommonServiceImpl.java
@@ -218,10 +218,10 @@ public class PromotionLotteryCommonServiceImpl implements PromotionLotteryCommon
             Map<String, String> dictMap) {
         String buyerCode = requestDTO.getMemberNo();
         String promotionId = promotionInfoDTO.getPromotionId();
-        //验证从汇林查出的粉丝号和前台传入的是否是一个值，防止有人篡改
+        //验证从汇邻查出的粉丝号和前台传入的是否是一个值，防止有人篡改
         if (!requestDTO.getMemberNo().equals(requestDTO.getOldMemberNo())) {
             throw new PromotionCenterBusinessException(ResultCodeEnum.LOTTERY_MEMBER_NO_VALIDATE_FAIL.getCode(),
-                    "前台传入的和从汇林查出的不是同一个粉丝号 入参:" + JSON.toJSONString(requestDTO));
+                    "前台传入的和从汇邻查出的不是同一个粉丝号 入参:" + JSON.toJSONString(requestDTO));
         }
 
         //验证会员店是否参与本次抽奖活动

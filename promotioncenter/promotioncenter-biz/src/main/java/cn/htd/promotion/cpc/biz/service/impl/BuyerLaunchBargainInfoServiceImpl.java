9c2cecec4e8107c5038db146531102447d2f70e7
shihb <hb.shi@htd.cn>
2017-10-17 14:58:36 +0800

    删除无效引用

--- promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/impl/BuyerLaunchBargainInfoServiceImpl.java
+++ promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/impl/BuyerLaunchBargainInfoServiceImpl.java
@@ -12,6 +12,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
 import cn.htd.common.DataGrid;
 import cn.htd.common.Pager;
 import cn.htd.common.constant.DictionaryConst;
@@ -21,7 +24,6 @@ import cn.htd.promotion.cpc.biz.dao.BuyerLaunchBargainInfoDAO;
 import cn.htd.promotion.cpc.biz.dao.PromotionBargainInfoDAO;
 import cn.htd.promotion.cpc.biz.dao.PromotionInfoDAO;
 import cn.htd.promotion.cpc.biz.dao.PromotionInfoExtendDAO;
-import cn.htd.promotion.cpc.biz.dmo.BuyerBargainRecordDMO;
 import cn.htd.promotion.cpc.biz.dmo.BuyerLaunchBargainInfoDMO;
 import cn.htd.promotion.cpc.biz.dmo.PromotionBargainInfoDMO;
 import cn.htd.promotion.cpc.biz.handle.PromotionBargainRedisHandle;
@@ -40,11 +42,8 @@ import cn.htd.promotion.cpc.dto.request.BuyerBargainLaunchReqDTO;
 import cn.htd.promotion.cpc.dto.request.BuyerBargainRecordReqDTO;
 import cn.htd.promotion.cpc.dto.response.BuyerLaunchBargainInfoResDTO;
 import cn.htd.promotion.cpc.dto.response.PromotionBargainInfoResDTO;
-import cn.htd.promotion.cpc.dto.response.PromotionInfoDTO;
 import cn.htd.promotion.cpc.dto.response.PromotionExtendInfoDTO;
-
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import cn.htd.promotion.cpc.dto.response.PromotionInfoDTO;
 
 @Service("buyerLaunchBargainInfoService")
 public class BuyerLaunchBargainInfoServiceImpl implements BuyerLaunchBargainInfoService {

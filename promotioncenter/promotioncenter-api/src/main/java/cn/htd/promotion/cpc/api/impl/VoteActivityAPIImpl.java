6bbab6f4b97b739d004ab118b4cbf4710e392299
zhengyang <a3888970>
2017-10-18 15:50:05 +0800

    no message

--- promotioncenter/promotioncenter-api/src/main/java/cn/htd/promotion/cpc/api/impl/VoteActivityAPIImpl.java
+++ promotioncenter/promotioncenter-api/src/main/java/cn/htd/promotion/cpc/api/impl/VoteActivityAPIImpl.java
@@ -142,7 +142,7 @@ public class VoteActivityAPIImpl implements VoteActivityAPI {
             e.printStackTrace();
             logger.error("VoteActivityAPI方法saveVoteActivityMember调用出错，信息{}",e.getMessage());
 
-            result.setCode(ResultCodeEnum.ERROR.getMsg());
+            result.setCode(ResultCodeEnum.ERROR.getCode());
         }
         return result;
     }

dbc5a62d938d79bfad49e0e6ff255630ba754e3e
jiangkun <jiaboy20@sina.com>
2017-10-31 11:29:54 +0800

    2017双12活动，优惠券激活

--- marketcenter/marketcenter-common/src/main/java/cn/htd/marketcenter/common/constant/RedisConst.java
+++ marketcenter/marketcenter-common/src/main/java/cn/htd/marketcenter/common/constant/RedisConst.java
@@ -61,9 +61,6 @@ public class RedisConst {
     public static final String REDIS_BUYER_TIMELIMITED_NEED_SAVE_USELOG =
             "B2B_MIDDLE_BUYER_TIMELIMITED_NEED_SAVE_USELOG";
 
-	//刮刮乐订单相关信息
-	public static final String REDIS_LOTTERY_B2C_MIDDLE_LOTTERY_ORDER_INFO = "B2C_MIDDLE_LOTTERY_ORDER_INFO";
-            
     // Redis 限时购 活动数据
     public static final String REDIS_TIMELIMITED_PURCHASE_INDEX = "B2B_MIDDLE_TIMELIMITED_PURCHASE_INDEX";
 

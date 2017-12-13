633af5ba79103bae107a885655d78d797fe19a88
shihb <hb.shi@htd.cn>
2017-12-13 20:47:56 +0800

    砍价金额分配逻辑调整

--- promotioncenter/promotioncenter-common/src/main/java/cn/htd/promotion/cpc/common/util/BargainPriceSplit.java
+++ promotioncenter/promotioncenter-common/src/main/java/cn/htd/promotion/cpc/common/util/BargainPriceSplit.java
@@ -2,7 +2,6 @@ package cn.htd.promotion.cpc.common.util;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
@@ -10,139 +9,147 @@ import org.springframework.stereotype.Service;
 
 @Service("bargainPriceSplit")
 public class BargainPriceSplit {
-	/**
-	 * 每个红包都要有钱，最低不能低于1分
-	 */
-	private static final int MINMONEY =1;
-	/**
-	 * 这里为了避免某一个红包占用大量资金，我们需要设定非最后一个红包的最大金额，我们把他设置为红包金额平均值的N倍；
-	 */
-	private static final double TIMES =1.5;
-	
-	/**金额为分的格式 */  
+    /**
+     * 每个红包都要有钱，最低不能低于1分
+     */
+    private static final int MINMONEY = 1;
+    /**
+     * 这里为了避免某一个红包占用大量资金，我们需要设定非最后一个红包的最大金额，我们把他设置为红包金额平均值的N倍；
+     */
+    private static final double TIMES = 2;
+
+    /** 金额为分的格式 */
     public static final String CURRENCY_FEN_REGEX = "\\-?[0-9]+";
-    
-	/**
-	 * 拆分红包
-	 * @param money ：红包总金额
-	 * @param count ：个数
-	 * @return
-	 * @throws Exception 
-	 */
-	public List<String> splitRedPackets(int money,int count) throws Exception{
-		int maxMoney = (int)(money * 0.9);
-		//红包 合法性校验
-		if(!isRight(money,count)){
-			return null;
-		}
-		//红包列表
-		List<String> list =new ArrayList<String>();
-		//每个红包最大的金额为平均金额的Times 倍
-		int max =(int)(money*TIMES/count);
-		max = max>maxMoney ? maxMoney : max;
-		//分配红包
-		for (int i = 0; i < count; i++) {
-			int one = randomRedPacket(money,MINMONEY,max,count-i,maxMoney);
-			String oneStr = changeF2Y(String.valueOf(one));
-			list.add(oneStr);
-			money -=one;
-		}
-		// 重新打乱列表
-		Collections.shuffle(list);
-		return list;
-	}
-	
-	/**
-	 * 随机分配一个红包
-	 * @param money
-	 * @param minS :最小金额
-	 * @param maxS ：最大金额(每个红包的默认Times倍最大值)
-	 * @param count
-	 * @param maxMoney : 金额最大限制
-	 * @return
-	 */
-	private int randomRedPacket(int money, int minS, int maxS, int count, int maxMoney) {
-		//若是只有一个，直接返回红包
-		if(count==1){
-			return money;
-		}
-		//若是最小金额红包 == 最大金额红包， 直接返回最小金额红包
-		if(minS ==maxS){
-			return minS;
-		}
-		//校验 最大值 max 要是比money 金额高的话？ 去 money 金额
-		int max = maxS>money ? money : maxS;
-		//随机一个红包 = 随机一个数* (金额-最小)+最小
-		int one =((int)Math.rint(Math.random()*(max-minS)+minS));
-		//剩下的金额
-		int moneyOther =money-one;
-		//校验这种随机方案是否可行，不合法的话，就要重新分配方案
-		if(isRight(moneyOther, count-1)){
-			return one;
-		}else{
-			//重新分配
-			double avg =moneyOther /(count-1);
-			//本次红包过大，导致下次的红包过小；如果红包过大，下次就随机一个小值到本次红包金额的一个红包
-			if(avg<MINMONEY){
-				 //递归调用，修改红包最大金额  
-				return randomRedPacket(money, minS, one, count, maxMoney);
-				
-			}else if(avg>maxMoney){
-				 //递归调用，修改红包最小金额  
-				return randomRedPacket(money, one, maxS, count, maxMoney);
-			}
-		}
-		return one;
-	}
-	/**
-	 * 红包 合法性校验
-	 * @param money
-	 * @param count
-	 * @return
-	 */
-	private boolean isRight(int money, int count) {
-		double avg =money/count;
-		//小于最小金额
-		if(avg<MINMONEY){
-			return false;
-		} 
-		return true;
-	}
-	
-	/** 
-     * 将分为单位的转换为元 （除100） 
-     *  
-     * @param amount 
-     * @return 
-     * @throws Exception  
-     */  
-    public static String changeF2Y(String amount) throws Exception{  
-        if(!amount.matches(CURRENCY_FEN_REGEX)) {  
-            throw new Exception("金额格式有误");  
-        }  
-        String moneyStr = BigDecimal.valueOf(Long.valueOf(amount)).divide(new BigDecimal(100)).toString();  
-        if (StringUtils.isNumeric(moneyStr))
-        {
-        	return moneyStr + ".00";
-        }else{
-        	int index = moneyStr.indexOf(".");
-        	String moneySub = moneyStr.substring(index + 1, moneyStr.length() - 1);
-        	if(StringUtils.isEmpty(moneySub)){
-        		moneyStr = moneyStr + "0";
-        	}
-        	return moneyStr;
+
+    /**
+     * 拆分红包
+     * 
+     * @param money
+     *            ：红包总金额
+     * @param count
+     *            ：个数
+     * @return
+     * @throws Exception
+     */
+    public List<String> splitRedPackets(int money, int count) throws Exception {
+        // 每个红包最大的金额为平均金额的Times 倍
+        int maxMoney = (int) (money * TIMES / count);
+        // 红包 合法性校验
+        if (!isRight(money, count, MINMONEY, maxMoney)) {
+            return null;
         }
-    } 
-//	
-//	public static void main(String[] args) {
-//		//随机一个188.88  5个红包
-//		BargainPriceSplit dd = new BargainPriceSplit();
-//		//单位是分
-//		try {
-//			System.out.println(dd.splitRedPackets(20000, 30));
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
+        // 红包列表
+        List<String> list = new ArrayList<String>();
+        // 分配红包
+        for (int i = 0; i < count; i++) {
+            int one = randomRedPacket(money, MINMONEY, maxMoney, count - i, maxMoney);
+            String oneStr = changeF2Y(String.valueOf(one));
+            list.add(oneStr);
+            money -= one;
+        }
+        return list;
+    }
+
+    /**
+     * 随机分配一个红包
+     * 
+     * @param money
+     * @param minS
+     *            :最小金额
+     * @param maxS
+     *            ：最大金额(每个红包的默认Times倍最大值)
+     * @param count
+     * @param maxMoney
+     *            : 金额最大限制
+     * @return
+     */
+    private int randomRedPacket(int money, int minS, int maxS, int count, int maxMoney) {
+        // 若是只有一个，直接返回红包
+        if (count == 1) {
+            return money;
+        }
+        // 若是最小金额红包 == 最大金额红包， 直接返回最小金额红包
+        if (minS == maxS) {
+            return minS;
+        }
+        // 为了避免死循环，最大最小值相差1时直接返回最大值
+        if (minS + 1 == maxS) {
+            return maxS;
+        }
+        // 校验 最大值 max 要是比money 金额高的话？ 去 money 金额
+        int max = maxS > money ? money : maxS;
+        // 随机一个红包 = 随机一个数* (金额-最小)+最小
+        int one = (int) (Math.random() * (max - minS)) + minS;
+        // 剩下的金额
+        int moneyOther = money - one;
+        // 校验这种随机方案是否可行，不合法的话，就要重新分配方案
+        if (isRight(moneyOther, count - 1, minS, maxS)) {
+            return one;
+        } else {
+            // 重新分配
+            int avg = moneyOther / (count - 1);
+            // 本次红包过大，导致下次的红包过小；如果红包过大，下次就随机一个小值到本次红包金额的一个红包
+            if (avg < MINMONEY) {
+                // 递归调用，修改红包最大金额
+                return randomRedPacket(money, minS, one, count, maxMoney);
+
+            } else if (avg > maxMoney) {
+                // 递归调用，修改红包最小金额
+                return randomRedPacket(money, one, maxS, count, maxMoney);
+            }
+        }
+        return one;
+    }
+
+    /**
+     * 红包 合法性校验
+     * 
+     * @param money
+     * @param count
+     * @return
+     */
+    private boolean isRight(int money, int count, int min, int max) {
+        double avg = money / count;
+        // 小于最小金额或者大于最大金额
+        if (avg < min || avg > max) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 将分为单位的转换为元 （除100）
+     * 
+     * @param amount
+     * @return
+     * @throws Exception
+     */
+    public static String changeF2Y(String amount) throws Exception {
+        if (!amount.matches(CURRENCY_FEN_REGEX)) {
+            throw new Exception("金额格式有误");
+        }
+        String moneyStr = BigDecimal.valueOf(Long.valueOf(amount)).divide(new BigDecimal(100)).toString();
+        if (StringUtils.isNumeric(moneyStr)) {
+            return moneyStr + ".00";
+        } else {
+            int index = moneyStr.indexOf(".");
+            String moneySub = moneyStr.substring(index + 1, moneyStr.length() - 1);
+            if (StringUtils.isEmpty(moneySub)) {
+                moneyStr = moneyStr + "0";
+            }
+            return moneyStr;
+        }
+    }
+
+//    public static void main(String[] args) {
+//        // 随机一个188.88 5个红包
+//        BargainPriceSplit dd = new BargainPriceSplit();
+//        // 单位是分
+//        try {
+//            System.out.println(dd.splitRedPackets(30000, 300));
+//        } catch (Exception e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//    }
 }

073bf40a0fca6181a27aa218df8bc0fd1d9d5ec3
xuhui <>
2018-01-11 10:52:17 +0800

    砍价活动，未开始和正在进行当中的活动，可以修改参砍商品数量，结束后不能更改

--- promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/impl/PromotionBargainInfoServiceImpl.java
+++ promotioncenter/promotioncenter-biz/src/main/java/cn/htd/promotion/cpc/biz/service/impl/PromotionBargainInfoServiceImpl.java
@@ -644,7 +644,7 @@ public class PromotionBargainInfoServiceImpl implements
 					promotionBargainInfoList.add(bagainInfoDTO);
 				}
 				// 写入reids操作
-				if (dictionary.getValueByCode(
+				/*if (dictionary.getValueByCode(
 						DictionaryConst.TYPE_PROMOTION_STATUS,
 						DictionaryConst.OPT_PROMOTION_STATUS_NO_START).equals(
 						promotionInfoDTO.getStatus())) {
@@ -653,6 +653,16 @@ public class PromotionBargainInfoServiceImpl implements
 				} else {
 					promotionBargainRedisHandle.addBargainInfo2Redis(
 							promotionBargainInfoList, true);
+				}*/
+				if (dictionary.getValueByCode(
+						DictionaryConst.TYPE_PROMOTION_STATUS,
+						DictionaryConst.OPT_PROMOTION_STATUS_END).equals(
+						promotionInfoDTO.getStatus())) {
+					promotionBargainRedisHandle.addBargainInfo2Redis(
+							promotionBargainInfoList, true);
+				} else {
+					promotionBargainRedisHandle.addBargainInfo2Redis(
+							promotionBargainInfoList, false);
 				}
 				result.setResult(updateResult);
 			}

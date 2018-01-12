e1089f93b4d30e5a609b9995c8597366aaf534d7
jiangkun <jiaboy20@sina.com>
2018-01-12 17:44:08 +0800

    弹屏活动开发

---
+++ contentcenter/contentcenter-domain/src/main/java/com/bjucloud/contentcenter/domain/HomepagePopupTerminalAd.java
@@ -0,0 +1,106 @@
+package com.bjucloud.contentcenter.domain;
+
+import java.util.Date;
+
+public class HomepagePopupTerminalAd {
+
+    private Long id;
+
+    private Long adId;
+
+    private String terminalType;
+
+    private int deleteFlag;
+
+    private Long createId;
+
+    private String createName;
+
+    private Date createTime;
+
+    private Long modifyId;
+
+    private String modifyName;
+
+    private Date modifyTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAdId() {
+        return adId;
+    }
+
+    public void setAdId(Long adId) {
+        this.adId = adId;
+    }
+
+    public String getTerminalType() {
+        return terminalType;
+    }
+
+    public void setTerminalType(String terminalType) {
+        this.terminalType = terminalType;
+    }
+
+    public int getDeleteFlag() {
+        return deleteFlag;
+    }
+
+    public void setDeleteFlag(int deleteFlag) {
+        this.deleteFlag = deleteFlag;
+    }
+
+    public Long getCreateId() {
+        return createId;
+    }
+
+    public void setCreateId(Long createId) {
+        this.createId = createId;
+    }
+
+    public String getCreateName() {
+        return createName;
+    }
+
+    public void setCreateName(String createName) {
+        this.createName = createName;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Long getModifyId() {
+        return modifyId;
+    }
+
+    public void setModifyId(Long modifyId) {
+        this.modifyId = modifyId;
+    }
+
+    public String getModifyName() {
+        return modifyName;
+    }
+
+    public void setModifyName(String modifyName) {
+        this.modifyName = modifyName;
+    }
+
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+}
